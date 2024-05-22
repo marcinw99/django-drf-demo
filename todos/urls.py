045b1ebd2d8a2b2e1e6e7072b28097ebede8c39c
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-22 20:36:12 +0200

    feat: generic views for projects and todos

--- todos/urls.py
+++ todos/urls.py
@@ -4,7 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from todos import views
 
 urlpatterns = [
-    path('projects/', views.projects)
+    path('projects/', views.ProjectsAPIView.as_view()),
+    path('todos/', views.TodosAPIView.as_view()),
+    path('todos/<int:pk>/', views.TodoAPIView.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- todos/views.py
+++ todos/views.py
@@ -1,30 +1,19 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework import generics
 
 from todos.models import Todo, Project
 from todos.serializers import TodoSerializer, ProjectSerializer
 
 
-@api_view(['GET', 'POST'])
-def projects(request, format=None):
-    if request.method == "GET":
-        all_projects = Project.objects.all()
-        serializer = ProjectSerializer(all_projects, many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ProjectSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProjectsAPIView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
-# class ProjectsAPIView(APIView):
-#     def GET(self, request):
-#         all_projects = Project.objects.all()
-#         serializer = ProjectSerializer(all_projects, many=True)
-#         return Response(serializer.data)
+class TodosAPIView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
