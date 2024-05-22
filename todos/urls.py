69f24fb9cabcde95050ec0f5aa4aa9d0a49da463
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-22 20:15:40 +0200

    feat: api_view decorator

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from todos import views
 
 urlpatterns = [
     path('projects/', views.projects)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- todos/views.py
+++ todos/views.py
@@ -1,20 +1,30 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+# from rest_framework.views import APIView
+
 from todos.models import Todo, Project
 from todos.serializers import TodoSerializer, ProjectSerializer
 
 
-@csrf_exempt
-def projects(request):
+@api_view(['GET', 'POST'])
+def projects(request, format=None):
     if request.method == "GET":
         all_projects = Project.objects.all()
         serializer = ProjectSerializer(all_projects, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == "POST":
         data = JSONParser().parse(request)
         serializer = ProjectSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class ProjectsAPIView(APIView):
+#     def GET(self, request):
+#         all_projects = Project.objects.all()
+#         serializer = ProjectSerializer(all_projects, many=True)
+#         return Response(serializer.data)
